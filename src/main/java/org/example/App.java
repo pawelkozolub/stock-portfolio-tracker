18be5fec8ed2bd329b83fad0b2dfdf3f8b2a32ea
pawelkozolub <pkozolub@gmail.com>
2023-09-30 14:52:45 +0200

    add > main configuration for: ComponentScan, EnableWebMcv, EnableTransactionManagement, EnableJpaRepositories

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,15 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan
+@EnableWebMvc
+@EnableTransactionManagement
+@EnableJpaRepositories
+public class App {
 }
