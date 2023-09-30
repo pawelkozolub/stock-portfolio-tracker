c26bcd84464083037cbfb2787bb78e074125bb0d
pawelkozolub <pkozolub@gmail.com>
2023-09-30 14:55:59 +0200

    add bean configuration > ViewResolver

---
+++ src/main/java/org/example/configuration/WebConfig.java
@@ -0,0 +1,27 @@
+package org.example.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    // to handle static resources (css, pictures, etc.) need to override default method
+    // otherwise Spring will look for controller methods to deal with them which do not exist
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+}
