0fbed850c0bbcbf8da5075636d0ef4e73b07aae3
pawelkozolub <pkozolub@gmail.com>
2023-09-30 14:54:04 +0200

    AppInitializer with references for further Bean configuration

---
+++ src/main/java/org/example/configuration/AppInitializer.java
@@ -0,0 +1,29 @@
+package org.example.configuration;
+
+import org.example.App;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { App.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] { WebConfig.class };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        return new Filter[] { new CharacterEncodingFilter("UTF-8") };
+    }
+}
