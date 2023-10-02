365a61b3a9593457254e4684c84f030a5c4034fc
pawelkozolub <pkozolub@gmail.com>
2023-10-02 22:50:31 +0200

    PortfolioController > /{portfolioId}/buy to avoid confusion with stock id

--- src/main/java/org/example/controller/PortfolioController.java
+++ src/main/java/org/example/controller/PortfolioController.java
@@ -1,24 +1,31 @@
 package org.example.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.example.model.Stock;
 import org.example.model.StockPortfolio;
 import org.example.service.StockPortfolioRepository;
+import org.example.service.StockRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+@Slf4j
 @Controller
 @RequestMapping("/portfolio")
 public class PortfolioController {
 
     private final StockPortfolioRepository portfolioRepository;
+    private final StockRepository stockRepository;
 
-    public PortfolioController(StockPortfolioRepository portfolioRepository) {
+    public PortfolioController(StockPortfolioRepository portfolioRepository, StockRepository stockRepository) {
         this.portfolioRepository = portfolioRepository;
+        this.stockRepository = stockRepository;
     }
 
     @GetMapping
@@ -38,4 +45,34 @@ public class PortfolioController {
         portfolioRepository.save(stockPortfolio);
         return "redirect:/portfolio";
     }
+
+    @GetMapping("/{id}")
+    public String view(Model model, @PathVariable(name = "id") Long id) {
+        StockPortfolio stockPortfolio = portfolioRepository.findById(id).orElse(null);
+        model.addAttribute("portfolio", stockPortfolio);
+        return "portfolio/portfolio-view";
+    }
+
+    @GetMapping("/{portfolioId}/buy")       // portfolioId to be used instead id > Spring confuses it with stock.id
+    public String addStock(Model model, @PathVariable(name = "portfolioId") Long id) {
+        StockPortfolio stockPortfolio = portfolioRepository.findById(id).orElse(null);
+        model.addAttribute("portfolio", stockPortfolio);
+        model.addAttribute("stock", new Stock());
+        return "portfolio/buy-stock-view";
+    }
+
+    @PostMapping("/{portfolioId}/buy")      // portfolioId to be used instead id > Spring confuses it with stock.id
+    public String saveStock(Stock stock, BindingResult result, @PathVariable(name = "portfolioId") Long id) {
+        if (result.hasErrors()) {
+            return "portfolio/buy-stock-view";
+        }
+        StockPortfolio stockPortfolio = portfolioRepository.findById(id).orElse(null);
+        if (stockPortfolio != null) {
+            stockRepository.save(stock);                // first persist entity -> stock
+            stockPortfolio.getStocks().add(stock);      // then add to list
+            portfolioRepository.save(stockPortfolio);   // finally update entity -> stockPortfolio
+        }
+        return "redirect:/portfolio/" + id;
+    }
+
 }
