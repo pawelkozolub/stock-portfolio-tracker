69e0212b2ab775a99694edaa73e3d34b6fe8f6cf
pawelkozolub <pkozolub@gmail.com>
2023-10-15 16:51:55 +0200

    add unit tests for calculateValue and calculateAveragePrice methods

--- src/test/java/org/example/model/BalanceTest.java
+++ src/test/java/org/example/model/BalanceTest.java
@@ -1,5 +1,6 @@
 package org.example.model;
 
+import com.google.common.truth.Truth;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -8,13 +9,63 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BalanceTest {
 
+    private Balance balance = new Balance();
+
     @Test
-    public void shouldCalculateValueUsingQuantityAndPrice() {
-        Balance balance = new Balance();
-        assertEquals(BigDecimal.valueOf(0.00), balance.calculateValue(0L, BigDecimal.valueOf(100.00)));
-        assertEquals(BigDecimal.valueOf(0.00), balance.calculateValue(10L, BigDecimal.valueOf(0.00)));
-        assertEquals(BigDecimal.valueOf(0.00), balance.calculateValue(0L, BigDecimal.valueOf(0.00)));
-        assertEquals(BigDecimal.valueOf(1000.0), balance.calculateValue(10L, BigDecimal.valueOf(100.00)));
+    public void givenNonZeroQuantityAndPrice_whenCalculateValue_thenNonZeroValue() {
+        Long quantity = 10L;
+        BigDecimal price = BigDecimal.valueOf(100.00);
+        BigDecimal expected = BigDecimal.valueOf(1000.00);
+        assertEquals(expected, balance.calculateValue(quantity, price));
+    }
+
+    @Test
+    public void givenNonZeroQuantityAndZeroPrice_whenCalculateValue_thenZeroValue() {
+        Long quantity = 10L;
+        BigDecimal price = BigDecimal.valueOf(0.00);
+        BigDecimal expected = BigDecimal.valueOf(0.00);
+        assertEquals(expected, balance.calculateValue(quantity, price));
+    }
+
+    @Test
+    public void givenZeroQuantityAndNonZeroPrice_whenCalculateValue_thenZeroValue() {
+        Long quantity = 0L;
+        BigDecimal price = BigDecimal.valueOf(100.00);
+        BigDecimal expected = BigDecimal.valueOf(0.00);
+        assertEquals(expected, balance.calculateValue(quantity, price));
+    }
+
+    @Test
+    public void givenZeroQuantityAndPrice_whenCalculateValue_thenZeroValue() {
+        Long quantity = 0L;
+        BigDecimal price = BigDecimal.valueOf(0.00);
+        BigDecimal expected = BigDecimal.valueOf(0.00);
+        assertEquals(expected, balance.calculateValue(quantity, price));
+    }
+
+    @Test
+    public void givenNonZeroQuantityAndValue_whenCalculateAveragePrice_thenNonZeroPrice() {
+        Long quantity = 4L;
+        BigDecimal value = BigDecimal.valueOf(10.00);
+        BigDecimal expected = BigDecimal.valueOf(2.50);
+        assertEquals(expected, balance.calculateAveragePrice(quantity, value));
+    }
+
+    @Test
+    public void givenNonZeroQuantityAndZeroValue_whenCalculateAveragePrice_thenZeroPrice() {
+        Long quantity = 4L;
+        BigDecimal value = BigDecimal.valueOf(0.00);
+        BigDecimal expected = BigDecimal.valueOf(0.00);
+        assertEquals(expected, balance.calculateAveragePrice(quantity, value));
+    }
+
+    @Test
+    public void givenZeroQuantity_whenCalculateAveragePrice_thenZeroPrice() {
+        Long quantity = 0L;
+        BigDecimal value = BigDecimal.valueOf(10.00);
+        IllegalArgumentException exception =
+                assertThrows(IllegalArgumentException.class, () -> balance.calculateAveragePrice(quantity, value));
+        Truth.assertThat(exception).hasMessageThat().isEqualTo("Average price for quantity = 0 cannot be determined");
     }
 
 }
