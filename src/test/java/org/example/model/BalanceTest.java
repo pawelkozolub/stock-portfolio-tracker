a8bd0a106c9e13c12fc5c018f0010b5ac7826653
pawelkozolub <pkozolub@gmail.com>
2023-10-17 21:53:48 +0200

    add tests for updateByBuyTransaction and updateBySellTransaction methods

--- src/test/java/org/example/model/BalanceTest.java
+++ src/test/java/org/example/model/BalanceTest.java
@@ -68,4 +68,76 @@ class BalanceTest {
         Truth.assertThat(exception).hasMessageThat().isEqualTo("Average price for quantity = 0 cannot be determined");
     }
 
+    @Test
+    public void givenBuyTransaction_whenUpdateByBuyTransaction_thenUpdateInitialValues() {
+        Balance bal = new Balance();
+
+        Transaction trans = new Transaction();
+        trans.setType(TransactionType.BUY.name());
+        trans.setQuantity(10L);
+        trans.setPrice(BigDecimal.valueOf(100.00));
+
+        bal.updateByBuyTransaction(trans);
+        assertEquals(10L, bal.getQuantity(), "quantity value");
+        assertEquals(BigDecimal.valueOf(100.00), bal.getAveragePrice(), "average price value");
+        assertEquals(BigDecimal.valueOf(1000.00), bal.getInvested(), "invested value");
+    }
+
+    @Test
+    public void givenBuyTransaction_whenUpdateByBuyTransaction_thenUpdateExistingValues() {
+        Balance bal = new Balance();
+        bal.setQuantity(10L);
+        bal.setAveragePrice(BigDecimal.valueOf(100.00));
+        bal.setInvested(BigDecimal.valueOf(1000.00));
+
+        Transaction trans = new Transaction();
+        trans.setType(TransactionType.BUY.name());
+        trans.setQuantity(10L);
+        trans.setPrice(BigDecimal.valueOf(200.00));
+
+        bal.updateByBuyTransaction(trans);
+        assertEquals(20L, bal.getQuantity(), "quantity value");
+        assertEquals(BigDecimal.valueOf(150.00), bal.getAveragePrice(), "average price value");
+        assertEquals(BigDecimal.valueOf(3000.00), bal.getInvested(), "invested value");
+    }
+
+    @Test
+    public void givenSellTransaction_whenUpdateBySellTransaction_thenUpdateExistingValues() {
+        Balance bal = new Balance();
+        bal.setQuantity(20L);
+        bal.setAveragePrice(BigDecimal.valueOf(100.00));
+        bal.setInvested(BigDecimal.valueOf(2000.00));
+
+        Transaction trans = new Transaction();
+        trans.setType(TransactionType.SELL.name());
+        trans.setQuantity(10L);
+        trans.setPrice(BigDecimal.valueOf(50.00));
+
+        bal.updateBySellTransaction(trans);
+        assertEquals(10L, bal.getQuantity(), "quantity value");
+        assertEquals(BigDecimal.valueOf(100.00), bal.getAveragePrice(), "average price value");
+        assertEquals(BigDecimal.valueOf(2000.00), bal.getInvested(), "invested value");
+        assertEquals(BigDecimal.valueOf(500.00), bal.getWithdrawn(), "withdrawn value");
+        assertEquals(BigDecimal.valueOf(-500.00), bal.getRealizedProfit(), "realizedProfit value");
+    }
+
+    @Test
+    public void givenSellAllTransaction_whenUpdateBySellTransaction_thenUpdateExistingValues() {
+        Balance bal = new Balance();
+        bal.setQuantity(20L);
+        bal.setAveragePrice(BigDecimal.valueOf(100.00));
+        bal.setInvested(BigDecimal.valueOf(2000.00));
+
+        Transaction trans = new Transaction();
+        trans.setType(TransactionType.SELL.name());
+        trans.setQuantity(20L);
+        trans.setPrice(BigDecimal.valueOf(50.00));
+
+        bal.updateBySellTransaction(trans);
+        assertEquals(0L, bal.getQuantity(), "quantity value");
+        assertEquals(BigDecimal.valueOf(0.00), bal.getAveragePrice(), "average price value");
+        assertEquals(BigDecimal.valueOf(2000.00), bal.getInvested(), "invested value");
+        assertEquals(BigDecimal.valueOf(1000.00), bal.getWithdrawn(), "withdrawn value");
+        assertEquals(BigDecimal.valueOf(-1000.00), bal.getRealizedProfit(), "realizedProfit value");
+    }
 }
